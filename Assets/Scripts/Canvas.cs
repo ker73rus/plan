f50e6112fc854076a49cc485bf6b198ab409c893
ker73rus <Ker73rus@yandex.ru>
2021-01-25 14:23:49 +0300

    hh

---
+++ Assets/Scripts/Canvas.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas : MonoBehaviour
+{
+    [SerializeField] GameObject text;
+    public void Deed(bool status)
+    {
+        if (status)
+            text.SetActive(true);
+        else
+            text.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] GameObject player, canvas;
+    [SerializeField] Vector3 dist;
+    [SerializeField] float gip;
+    public bool near;
+    public void Deed()
+    {
+        canvas.GetComponent<Canvas>().Deed(near);
+    }
+    void Update()
+    {
+        dist = player.transform.position - transform.position;
+        gip = Mathf.Sqrt(Mathf.Pow(dist.x, 2) + Mathf.Pow(dist.y, 2));
+        if (gip < 1.5f)
+        {
+            near = true;
+            Deed();
+        }
+        else
+        {
+            near = false;
+            Deed();
+        }
+    }
+}
